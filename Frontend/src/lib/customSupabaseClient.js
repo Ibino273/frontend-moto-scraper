0e093a85b6d4b9e38c354c69bd8ad118265fe1e5
Ibino273 <lorenzo.delgiudice.sc@gmail.com>
2025-08-01 17:12:43 +0200

    Delete Frontend directory

--- Frontend/src/lib/customSupabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://pulabpguqotbkmxwyygr.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1bGFicGd1cW90YmtteHd5eWdyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM2MzQ1MzUsImV4cCI6MjA2OTIxMDUzNX0.WOEaFVN4jXgU1nYnzdOTtVEozkJarYQwETj4YHBA4Ag';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
